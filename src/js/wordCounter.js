bc15ca0327060977be8ef5263e474854e69ff760
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-02-01 08:21:27 +0000

    Modularised code and added new script to index

---
+++ src/js/wordCounter.js
@@ -0,0 +1,57 @@
+// wordCounter.js
+
+/**
+ * Cleans and normalizes the input string by replacing line breaks, removing special characters,
+ * and replacing multiple whitespace with a single space.
+ *
+ * @param {string} str - The input string to be cleaned.
+ * @returns {string} The cleaned and normalized string.
+ */
+const cleanAndNormalize = (str) => {
+    if (!str) {
+        throw new Error(
+            "Please provide a value as an argument for the cleanAndNormalize function."
+        );
+    }
+
+    // Replace line breaks with space
+    str = str.replace(/(?:\r\n|\r|\n)/g, " ");
+
+    // Remove special characters except for alphanumeric, space, dot, and hyphen
+    str = str.replace(/[^a-zA-Z0-9\-.\s]/g, "");
+
+    // Replace multiple whitespace with a single space
+    str = str.replace(/\s+/g, " ");
+
+    return str.trim();
+};
+
+/**
+ * Counts the number of words in a cleaned and normalized string.
+ *
+ * @param {string} str - The cleaned and normalized string.
+ * @returns {number} The number of words in the string.
+ */
+const wordCount = (str) => {
+    return str.split(" ").length;
+};
+
+/**
+ * Updates the word count in the UI based on the input textarea value.
+ *
+ * @param {string} textareaId - The ID of the textarea element.
+ * @param {string} wordCountElementId - The ID of the element to display the word count.
+ */
+const updateWordCount = (textareaId, wordCountElementId) => {
+    const elWordCountTextarea = document.getElementById(textareaId);
+    const elWordCountValue = document.getElementById(wordCountElementId);
+
+    const cleanedText = cleanAndNormalize(elWordCountTextarea.value);
+    const numberOfWords = wordCount(cleanedText);
+
+    elWordCountValue.innerText = numberOfWords;
+
+    // Update the class based on the number of words
+    elWordCountValue.classList.toggle("text-danger", numberOfWords < 1);
+    elWordCountValue.classList.toggle("text-success", numberOfWords >= 1);
+};
